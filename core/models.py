e4d52f656cb43a4ad182a47ae0c7e4d598a5479d
DayanCastro <81377718+dayancastro16@users.noreply.github.com>
2024-06-26 23:17:22 -0400

    usuario admin

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
+    
