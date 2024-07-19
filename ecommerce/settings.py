ba7e10496218da1f31372fd80d52d0255df647c0
Ignacio <rignacio871@gmail.com>
2024-07-18 21:54:13 -0400

    cambios

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     #custom apps
     'jazzmin',
     'core',
+    'userauths',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
